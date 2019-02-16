4ef90cd9e1bfc9493d2d252345574ee8cbb3de67
pial08 <pialcsedu19@gmail.com>
2019-02-17 01:57:36 +0600

    Default Changelist

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function (req, res) {
-    //res.send('Hello World!')
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 });
 
 app.post('/', function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
     request(url, function (err, response, body) {
         if(err){
             res.render('index', {weather: null, error: 'Error, please try again'});
@@ -41,3 +40,45 @@ app.listen(3000, function () {
 
 
 
+
+
+/*
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = 'c215cca16ac090ccf15e4e79e236a152';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    request(url, function (err, response, body) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weather = JSON.parse(body)
+            if(weather.main == undefined){
+                res.render('index', {weather: null, error: 'Error, please try again'});
+            } else {
+                let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
+
+*/
